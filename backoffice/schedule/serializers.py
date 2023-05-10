e6e2e03bdab2f645899d4d26d1fab8a20a74eaf9
straipe <43769778+straipe@users.noreply.github.com>
2023-05-11 00:11:26 +0900

    Delete backoffice directory

--- backoffice/schedule/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Movie
-        fields = ('movie_no','title','running_time',
-                  'descriptions','genre')
-# DB instance => JSON data
