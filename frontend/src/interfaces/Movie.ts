2cb4f3d6c43e5199fb0bd858048523c7b242f542
wldnd9904 <wldnd9904@uos.ac.kr>
2023-05-27 16:54:10 +0900

    movie interface

--- frontend/src/interfaces/Movie.ts
+++ frontend/src/interfaces/Movie.ts
@@ -3,12 +3,13 @@ export default interface IMovie{
     mov_no: number|string;
     mov_nm: string;
     run_time_min: number|string;
-    grade_no: number|string;
+    mov_grade_no: number|string;
     gen_no: number|string;
     dir_nm: string;
     act_nm: string;
     mov_detail: string;
     distributor: string;
-    lan: string;
+    lang: string;
     image_url: string;
+    release_date:Date;
 };
--- frontend/src/utils/demos.ts
+++ frontend/src/utils/demos.ts
@@ -68,40 +68,43 @@ export const demoMovie:IMovie = {
     mov_no: 87032,
     mov_nm: "너의 이름은",
     run_time_min: 120,
-    grade_no: "12",
+    mov_grade_no: "12",
     gen_no: "애니메이션",
     dir_nm: "신카이 마코토",
     act_nm: "미츠하, 타키쿤",
     mov_detail: "아직 만난 적 없는 너를, 찾고 있어.\nまだ会ったことのない君を、探している。",
     distributor: "TOHO",
-    lan: "Japanese",
+    lang: "Japanese",
     image_url: "https://github.com/wldnd9904/DB_BoxOffice/blob/master/frontend/images/posters/87032_320.jpg?raw=true",
+    release_date: new Date()
 };
 export const demoMovie2:IMovie = {
     mov_no: 87002,
     mov_nm: "문재인",
     run_time_min: 120,
-    grade_no: "18",
     gen_no: "애니메이션",
     dir_nm: "신카이 마코토",
     act_nm: "미츠하, 타키쿤",
     mov_detail: "아직 만난 적 없는 너를, 찾고 있어.\nまだ会ったことのない君を、探している。",
     distributor: "TOHO",
-    lan: "Japanese",
+    lang: "Japanese",
     image_url: "https://github.com/wldnd9904/DB_BoxOffice/blob/master/frontend/images/posters/87002_320.jpg?raw=true",
+    mov_grade_no: "3",
+    release_date: new Date()
 };
 export const demoMovie3:IMovie = {
     mov_no: 86995,
     mov_nm: "짱구",
     run_time_min: 120,
-    grade_no: "all",
+    mov_grade_no: "all",
     gen_no: "애니메이션",
     dir_nm: "신카이 마코토",
     act_nm: "미츠하, 타키쿤",
     mov_detail: "아직 만난 적 없는 너를, 찾고 있어.\nまだ会ったことのない君を、探している。",
     distributor: "TOHO",
-    lan: "Japanese",
+    lang: "Japanese",
     image_url: "https://github.com/wldnd9904/DB_BoxOffice/blob/master/frontend/images/posters/86995_320.jpg?raw=true",
+    release_date: new Date()
 };
 export const demoMovies:IMovie[] =[
     demoMovie,demoMovie2,demoMovie3,
