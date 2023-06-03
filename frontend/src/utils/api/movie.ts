9040721b98613846e1d5e1dbe13fa2748c3f469f
wldnd9904 <wldnd9904@uos.ac.kr>
2023-06-03 20:20:49 +0900

    movie add/delete done

--- frontend/src/utils/api/movie.ts
+++ frontend/src/utils/api/movie.ts
@@ -17,19 +17,19 @@ export async function getMovieListAPI():Promise<IMovie[]> {
 }
 export async function addMovieAPI() {
     if(demo)return {result:"ok"};
-    const ggangtong:IMovie ={
+    const ggangtong ={
         mov_no: 10000,
         mov_nm: "새 영화",
         run_time_min: 120,
-        mov_grade_no: 1,
+        mov_grade_no: "CD00200",
         dir_nm: "감독명",
         act_nm: "배우명(들)",
         mov_detail: "설명",
         distributor: "배급사",
         lang: "언어",
         image_url: "이미지URL",
-        gen_no: 1,
-        release_date:new Date()
+        gen_no: "CD00100",
+        release_date:"2000-01-01"
     };
     let message = await axios.post(BASE_URL+"/movie/",ggangtong).then((response)=>response.data).catch((error)=>error);
     console.log(message);
@@ -42,8 +42,9 @@ export async function deleteMovieAPI(mov_no:number|string) {
     return data;
 }
 export async function editMovieAPI(data:IMovie) {
+    console.log(data);
     if(demo)return {result:"ok"};
-    let message = await axios.post(BASE_URL+"/movieedit",{data},{headers:{'Content-Type':'application/x-www-form-urlencoded'}}).then((response)=>response.data).catch((error)=>error);
+    let message = await axios.put(BASE_URL+`/movie/${data.mov_no}`,data,{headers:{'Content-Type':'application/x-www-form-urlencoded'}}).then((response)=>response.data).catch((error)=>error);
     console.log(message);
     return message;
 }
