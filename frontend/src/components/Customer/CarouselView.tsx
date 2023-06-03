3a91e1af87de4a96af064f8dca1418fc5e764335
wldnd9904 <wldnd9904@uos.ac.kr>
2023-06-03 21:31:00 +0900

    video thing

--- frontend/src/components/Customer/CarouselView.tsx
+++ frontend/src/components/Customer/CarouselView.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import styled from 'styled-components';
+
+const CarouselItem = styled(Carousel.Item)`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  video {
+    width:100%;
+    height:100%;
+  }
+`;
 
 function CarouselView() {
   const [index, setIndex] = useState(0);
@@ -10,43 +21,28 @@ function CarouselView() {
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.dominos.co.kr/admin/upload/banner/20221031_8v65FwqK.jpg"
-          alt="First slide"
-        />
+      <CarouselItem>
+          {index==0?<video autoPlay muted>
+            <source src="https://adimg.cgv.co.kr/images/202305/Transformers7/0601_TF7_1080x608.mp4" type="video/mp4" />
+            이 브라우저는 Video 태그를 지원하지 않습니다. (Your browser does not support the video tag.)
+          </video>:null}
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>트랜스포머: 비스트의 서막</h3>
+          <p>★6월 6일, 전세계 최초 개봉★</p>
         </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.dominos.co.kr/admin/upload/banner/20221031_8v65FwqK.jpg"
-          alt="Second slide"
-        />
+      </CarouselItem>
 
+      <CarouselItem>
+        {index==1?<video autoPlay muted>
+            <source src="http://h.vod.cgv.co.kr:80/vodCGVa/87045/87045_214957_1200_128_960_540.mp4" type="video/mp4" />
+            이 브라우저는 Video 태그를 지원하지 않습니다. (Your browser does not support the video tag.)
+          </video>:null}
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>범죄도시 3</h3>
+          <p>나쁜 놈들 잡는 데 이유 없고 제한 없다. 커진 판도 시원하게 싹 쓸어버린다!</p>
         </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.dominos.co.kr/admin/upload/banner/20221031_8v65FwqK.jpg"
-          alt="Third slide"
-        />
+      </CarouselItem>
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
     </Carousel>
   );
 }
